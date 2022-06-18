b7fb4a6c9b25d6597e9f7b0d53352050b669ff0d
borgmanJeremy <46930769+borgmanJeremy@users.noreply.github.com>
2022-06-17 19:28:56 -0500

    v1 ready to go

--- tests/test_flatex.py
+++ tests/test_flatex.py
@@ -1,5 +1,6 @@
 import unittest
-from .flatex import get_included_file_name, parse_args
+from flatex.main import parse_args, get_included_file_name
+
 
 class TestParse(unittest.TestCase):
     def test_verbose(self):
@@ -21,11 +22,9 @@ class TestIncludedFileName(unittest.TestCase):
         res = get_included_file_name(r"    \input{file} %comment")
         self.assertEqual("file.tex", res)
 
-
     def test_error_case(self):
         res = get_included_file_name(r"file")
         self.assertEqual("", res)
-
 
 
 if __name__ == "__main__":
